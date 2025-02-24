e7cf2d46c4e78b3d754357b7fcd30af6a8e23811
STOK3 <67011392@kmitl.ac.th>
2025-02-25 00:09:51 +0700

    for ham

---
+++ client/app/signin/page.tsx
@@ -0,0 +1,18 @@
+export default function SignInPage() {
+    return (
+        <div>
+            <h1>Sign in</h1>
+            <form>
+                <label>
+                    Email
+                    <input type="email" />
+                </label>
+                <label>
+                    Password
+                    <input type="password" />
+                </label>
+                <button type="submit">Sign in</button>
+            </form>
+        </div>
+    )
+}
