dc0993d7d85f678d79063f3e11172ad4f4e40be4
STOK3 <67011392@kmitl.ac.th>
2025-02-25 00:19:08 +0700

    flex

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -1,9 +1,9 @@
 export default function SignInPage() {
     return (
-        <div>
-            <h1>Sign in</h1>
-            <form>
-                <label className="text-blue-200">
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 >Sign in</h1>
+            <form className="flex flex-col gap-10">
+                <label>
                     Email
                     <input type="email" />
                 </label>
